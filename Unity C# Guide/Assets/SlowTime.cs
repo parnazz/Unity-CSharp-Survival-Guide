c61dae3565cfeda133561b27c02af1ceac61988a
parnazz <parnaz@mail.ru>
2019-11-01 20:56:38 +0300

    Add slowing the time ability

---
+++ Unity C# Guide/Assets/SlowTime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowTime : MonoBehaviour
+{
+    [SerializeField]
+    private float _timeScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = _timeScale;
+        }
+    }
+}
