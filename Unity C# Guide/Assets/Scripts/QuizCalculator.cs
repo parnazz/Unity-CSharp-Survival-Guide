347982eeeafb49fd28eff31618fbf53e4c026679
parnazz <parnaz@mail.ru>
2019-11-03 13:29:25 +0300

    QuizeCalculator Challange

---
+++ Unity C# Guide/Assets/Scripts/QuizCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizCalculator : MonoBehaviour
+{
+    /*[SerializeField]
+    private float[] _quizes;
+
+    public float _avgVal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < _quizes.Length; i++)
+        {
+            _quizes[i] = Random.Range(0f, 10f);
+            _avgVal += _quizes[i];
+        }
+
+        _avgVal /= _quizes.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
