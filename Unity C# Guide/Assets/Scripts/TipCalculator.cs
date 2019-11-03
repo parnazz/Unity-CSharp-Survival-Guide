4898d15675f10668e51169ac33456c10ce700577
parnazz <parnaz@mail.ru>
2019-11-03 13:13:23 +0300

    TipCalculator challange

---
+++ Unity C# Guide/Assets/Scripts/TipCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipCalculator : MonoBehaviour
+{
+    /*[SerializeField]
+    private float _bill;
+    [SerializeField]
+    private float _tipAmount;
+    [SerializeField]
+    private float _totalAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _tipAmount = _bill * 0.2f;
+        _totalAmount = _bill + _tipAmount;
+        Debug.Log("Your bill is: " + _bill);
+        Debug.Log("You must pay at least 20% tip. Your tip is: " + _tipAmount);
+        Debug.Log("Your your total check: " + _totalAmount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
