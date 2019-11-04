d91b27d846f50bead023e7fbd86ef6557cf09716
parnazz <parnaz@mail.ru>
2019-11-04 12:38:37 +0300

    IfTest1

---
+++ Unity C# Guide/Assets/Scripts/IfTest1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class IfTest1 : MonoBehaviour
+{
+    int _score = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _score += 10;
+            Debug.Log(_score);
+        }
+    }
+}
--- Unity C# Guide/Assets/Scripts/QuizCalculator.cs
+++ Unity C# Guide/Assets/Scripts/QuizCalculator.cs
@@ -7,7 +7,7 @@ public class QuizCalculator : MonoBehaviour
     /*[SerializeField]
     private float[] _quizes;
 
-    public float _avgVal;
+    public float avgVal;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +15,10 @@ public class QuizCalculator : MonoBehaviour
         for (int i = 0; i < _quizes.Length; i++)
         {
             _quizes[i] = Random.Range(0f, 10f);
-            _avgVal += _quizes[i];
+            avgVal += _quizes[i];
         }
 
-        _avgVal /= _quizes.Length;
+        avgVal /= _quizes.Length;
     }
 
     // Update is called once per frame
