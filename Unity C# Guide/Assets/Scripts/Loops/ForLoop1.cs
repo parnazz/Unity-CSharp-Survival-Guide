94d3f8d74acb12af7c0bc9619991e03535e26582
parnazz <parnaz@mail.ru>
2019-11-04 16:58:44 +0300

    Loops completed

---
+++ Unity C# Guide/Assets/Scripts/Loops/ForLoop1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoop1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Numbers from 1 to 10:");
+        for (int i = 0; i <= 10; i++)
+        {
+            Debug.Log("i = " + i);
+        }
+
+        Debug.Log("Even numbers from 1 to 20:");
+        for (int i = 0; i <= 20; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Debug.Log("i = " + i);
+            }
+        }
+
+        Debug.Log("Odd numbers from 1 to 30:");
+        for (int i = 0; i <= 30; i++)
+        {
+            if (i % 2 == 1)
+            {
+                Debug.Log("i = " + i);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity C# Guide/Assets/Scripts/Loops/WhileLoop1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoop1 : MonoBehaviour
+{
+    private float _speed = 0;
+    private float _maxSpeed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _maxSpeed = Random.Range(60, 120);
+
+        StartCoroutine(SpeedIncrement());
+    }
+
+    IEnumerator SpeedIncrement()
+    {
+        while (_speed <= _maxSpeed)
+        {
+            _speed += 5;
+            Debug.Log("Speed = " + _speed);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
