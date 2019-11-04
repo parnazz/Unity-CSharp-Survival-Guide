54e55cb633360ce9f86ffd08e283af854f2ec084
parnazz <parnaz@mail.ru>
2019-11-04 16:43:53 +0300

    If and Switch statements completed

---
+++ Unity C# Guide/Assets/Scripts/If Statment/IfTest1.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Switch2 : MonoBehaviour
+{
+    int _score = 0;
+    [SerializeField]
+    private GameObject _player;
+    private Renderer _playerRenderer;
+
+    private void Start()
+    {
+        _playerRenderer = _player.GetComponent<Renderer>();
+
+        _playerRenderer.material.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            _score = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            _score = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            _score = 3;
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            _score = 4;
+        }
+       
+        switch (_score)
+        {
+            case 1:
+                _playerRenderer.material.color = Color.red;
+                break;
+            case 2:
+                _playerRenderer.material.color = Color.green;
+                break;
+            case 3:
+                _playerRenderer.material.color = Color.blue;
+                break;
+            case 4:
+                _playerRenderer.material.color = Color.black;
+                break;
+            default:
+                _playerRenderer.material.color = Color.white;
+                break;
+        }
+    }
+}
---
+++ Unity C# Guide/Assets/Scripts/Switch Statments/Switch3.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch3 : MonoBehaviour
+{
+    int _weaponID;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _weaponID = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _weaponID = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _weaponID = 3;
+        }
+
+        switch(_weaponID)
+        {
+            case 1:
+                Debug.Log("You selected knife");
+                break;
+            case 2:
+                Debug.Log("You selected pistol");
+                break;
+            case 3:
+                Debug.Log("You selected machine gun");
+                break;
+            default:
+                _weaponID = 1;
+                break;
+        }
+    }
+}
