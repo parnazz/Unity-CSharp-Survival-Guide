bc4defa0836deef8aa17f3968c9dc7c5704e4ae2
parnazz <parnaz@mail.ru>
2019-11-05 21:28:48 +0300

    Arrays Challenge 1-2

---
+++ Unity C# Guide/Assets/Scripts/Arrays/Array1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array1 : MonoBehaviour
+{
+    public string[] names;
+    public int[] ages;
+    public string[] colors;
+
+    int _randomIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _randomIndex = Random.Range(0, names.Length);
+
+        Debug.Log(names[_randomIndex]);
+        Debug.Log(ages[_randomIndex]);
+        Debug.Log(colors[_randomIndex]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(names[_randomIndex]);
+            Debug.Log(ages[_randomIndex]);
+            Debug.Log(colors[_randomIndex]);
+        }
+    }
+}
