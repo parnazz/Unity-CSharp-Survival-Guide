dd7662228afbc8837fa77ba31ef30f8e5b05a7d2
parnazz <parnaz@mail.ru>
2019-11-07 21:38:41 +0300

    Loop Challenges completed

---
+++ Unity C# Guide/Assets/Scripts/Loops/ForLoop2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoop2 : MonoBehaviour
+{
+    public GameObject[] cubes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int i = 0; i < cubes.Length; i++)
+            {
+                cubes[i].GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+        }
+    }
+}
