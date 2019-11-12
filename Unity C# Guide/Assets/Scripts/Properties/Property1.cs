c121c04dcac4ccae52d42d776e88c4c1826bdc31
parnazz <parnaz@mail.ru>
2019-11-12 21:11:27 +0300

    Properties completed

---
+++ Unity C# Guide/Assets/Scripts/Properties/Property1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Property1 : MonoBehaviour
+{
+    static float _speed = 5f;
+
+    static float Speed
+    {
+        get
+        {
+            return _speed;
+        }
+    }
+
+    public string Name { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Name = "Josh";
+
+        Debug.Log("Name: " + Name);
+        Debug.Log("Speed: " + Speed);
+    }
+
+}
