1b13e26452171ca522ab437f2a81d890138d87a4
parnazz <parnaz@mail.ru>
2019-11-13 21:13:39 +0300

    Lists completed

---
+++ Unity C# Guide/Assets/Scripts/Lists/List1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class List1 : MonoBehaviour
+{
+    public List<string> names = new List<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintNames();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && names.Count > 0)
+        {
+            DeleteRandomName();
+            PrintNames();
+        }
+    }
+
+    void PrintNames()
+    {
+        foreach (var name in names)
+        {
+            Debug.Log(name);
+        }
+    }
+
+    void DeleteRandomName()
+    {
+        int randomIndex = Random.Range(0, names.Count);
+
+        names.RemoveAt(randomIndex);
+    }
+}
---
+++ Unity C# Guide/Assets/Scripts/Lists/List2.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class List2 : MonoBehaviour
+{
+    public List<GameObject> objects = new List<GameObject>();
+    public List<GameObject> createdObjects = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InstantiateObjects();
+
+            if (createdObjects.Count >= 10)
+            {
+                ChangeColor();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            DeleteAllCreatedObjects();
+        }
+    }
+
+    void InstantiateObjects()
+    {
+        foreach (var obj in objects)
+        {
+            if (createdObjects.Count < 10)
+            {
+                Vector3 randomPos = new Vector3(Random.Range(-7f, 7f), Random.Range(-5f, 5f), 0);
+
+                GameObject createdObj = Instantiate(obj, randomPos, Quaternion.identity);
+                createdObjects.Add(createdObj);
+            }
+        }
+    }
+
+    void ChangeColor()
+    {
+        foreach (var obj in createdObjects)
+        {
+            obj.GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+    }
+
+    void DeleteAllCreatedObjects()
+    {
+        foreach (var obj in createdObjects)
+        {
+            Destroy(obj);
+        }
+
+        createdObjects.Clear();
+    }
+}
