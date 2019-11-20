563fbcd47ff8c8d47c8ee2ae20e7bf876b573ab1
parnazz <parnaz@mail.ru>
2019-11-20 21:24:05 +0300

    Linq completed

---
+++ Unity C# Guide/Assets/Scripts/LINQ/LINQ1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class LINQ1 : MonoBehaviour
+{
+    public int[] quizGrades = new int[10];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < quizGrades.Length; i++)
+        {
+            quizGrades[i] = Random.Range(0, 101);
+        }
+
+        var result = quizGrades.Where(grade => grade > 69).OrderBy(grade => grade);
+
+        foreach (var grade in result)
+        {
+            Debug.Log(grade);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity C# Guide/Assets/Scripts/LINQ/LINQ2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public int itemID;
+    public int attackModifier;
+}
+
+public class LINQ2 : MonoBehaviour
+{
+    public List<Item> items;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var idCheck = items.Where(id => id.itemID == 3);
+        var attackCheck = items.Where(attack => attack.attackModifier > 35);
+        var avg = items.Average(item => item.attackModifier);
+
+        foreach (var item in idCheck)
+        {
+            Debug.Log("Item with ID = 3: " + item.name);
+        }
+
+        foreach (var item in attackCheck)
+        {
+            Debug.Log(item.name + " has attack modifier greater than 35");
+        }
+
+        Debug.Log("Average attack modifier is: " + avg);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
