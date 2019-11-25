91593be0b7d1607ae4debb49d6b4b546268c3d00
parnazz <parnaz@mail.ru>
2019-11-25 22:48:37 +0300

    Singletone completed

---
+++ Unity C# Guide/Assets/Scripts/Patterns/Challenge1/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private static SpawnManager _instance;
+
+    public static SpawnManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.Log("SpawnManager is null");
+
+            return _instance;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    public void StartSpawn(GameObject cube)
+    {
+        Vector3 randPos = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
+        Instantiate(cube, randPos, Quaternion.identity);
+    }
+}
---
+++ Unity C# Guide/Assets/Scripts/Patterns/Challenge1/SpawnStarter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStarter : MonoBehaviour
+{
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawnCoroutine());
+    }
+
+    IEnumerator StartSpawnCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3f);
+            SpawnManager.Instance.StartSpawn(cube);
+        }
+    }
+}
